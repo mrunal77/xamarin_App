3f3e4e8232b1f0a4fbbb8dc15ed7b6d29c3aaeb7
mrunal77 <mtmeshram77@outlook.com>
2019-07-13 18:44:16 +0530

    Project created.

---
+++ WeatherApp/WeatherApp.Android/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -0,0 +1,14 @@
+package mono.android.app;
+
+public class XamarinAndroidEnvironmentVariables
+{
+	// Variables are specified the in "name", "value" pairs
+	public static final String[] Variables = new String[] {
+		"MONO_LOG_LEVEL", "info",
+		"XAMARIN_BUILD_ID", "47d6b3c1-ac5d-46be-acce-2dcca2fe578f",
+		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
+		"XA_TLS_PROVIDER", "btls",
+		"MONO_GC_PARAMS", "major=marksweep-conc",
+
+	};
+}
